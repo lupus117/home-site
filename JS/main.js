1d994554df0674260428364aaf060c72a8ec6121
lupus117 <stenunt117@gmail.com>
2024-07-27 22:38:23 +0300

    Add code for creating desktop items

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,23 @@
-function elementFromHtml(html){
-    const template = document.createElement;
+
+
+
+function createIdItem(Id, img, desc, link) {
+    var tmpitem = `
+<div class="item">
+    <a href="${link}"><img src="${img}" alt="token"></a>
+    <p>${desc}</p>
+
+</div>`;
+
+   var div = document.getElementById(Id).insertAdjacentHTML("beforeend", tmpitem.trim());
+    console.log(div);
     
+
+}
+
+
+window.onload = init;
+function init(){
+    createIdItem("desktop", "images/icons/icons8-folder-480.png", "Lupus", "#");
+
 }

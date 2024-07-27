5f4b82cce6f346dc15bcc8327de41494878ca7c6
lupus117 <stenunt117@gmail.com>
2024-07-27 23:48:24 +0300

    Add temporary window to work on windows

--- JS/main.js
+++ JS/main.js
@@ -1,23 +1,37 @@
 
 
 
-function createIdItem(Id, img, desc, link) {
+function createIdItem(Id, img, desc, func) {
     var tmpitem = `
-<div class="item">
-    <a href="${link}"><img src="${img}" alt="token"></a>
+<div class="item" onclick="${func}">
+    <a><img src="${img}" alt="token"></a>
     <p>${desc}</p>
 
 </div>`;
 
-   var div = document.getElementById(Id).insertAdjacentHTML("beforeend", tmpitem.trim());
-    console.log(div);
-    
+   var div = document.getElementById(Id).insertAdjacentHTML("beforeend", tmpitem.trim());    
 
 }
 
 
 window.onload = init;
 function init(){
-    createIdItem("desktop", "images/icons/icons8-folder-480.png", "Lupus", "#");
-
+    createIdItem("desktop", "images/icons/folder.png", "all", "console.log('1')");
+    createIdItem("desktop", "images/icons/folder.png", "Lupus", "");
+    createIdItem("desktop", "images/icons/sten.png", "Lupus", "");
+    createIdItem("windowcontent", "images/icons/folder.png", "all", "console.log('1')");
+    createIdItem("windowcontent", "images/icons/folder.png", "Lupus", "");
+    createIdItem("windowcontent", "images/icons/sten.png", "Lupus", "");
+    createIdItem("windowcontent", "images/icons/folder.png", "all", "console.log('1')");
+    createIdItem("windowcontent", "images/icons/folder.png", "Lupus", "");
+    createIdItem("windowcontent", "images/icons/sten.png", "Lupus", "");
+        createIdItem("windowcontent", "images/icons/folder.png", "all", "console.log('1')");
+    createIdItem("windowcontent", "images/icons/folder.png", "Lupus", "");
+    createIdItem("windowcontent", "images/icons/sten.png", "Lupus", "");
+    createIdItem("windowcontent", "images/icons/folder.png", "all", "console.log('1')");
+    createIdItem("windowcontent", "images/icons/folder.png", "Lupus", "");
+    createIdItem("windowcontent", "images/icons/sten.png", "Lupus", "");
+    createIdItem("windowcontent", "images/icons/folder.png", "all", "console.log('1')");
+    createIdItem("windowcontent", "images/icons/folder.png", "Lupus", "");
+    createIdItem("windowcontent", "images/icons/sten.png", "Lupus", "");
 }

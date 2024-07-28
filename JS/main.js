9b4ceed2ca688d650c889a471ea970c6b04ec72c
lupus117 <stenunt117@gmail.com>
2024-07-28 19:12:01 +0300

    add window closing button

--- JS/main.js
+++ JS/main.js
@@ -13,27 +13,31 @@ function createIdItem(Id, img, desc, func, link) {
 
 </div>`;
 
-   var div = document.getElementById(Id).insertAdjacentHTML("beforeend", tmpitem.trim());    
+    var div = document.getElementById(Id).insertAdjacentHTML("beforeend", tmpitem.trim());
 
 }
 
-function openWindow(){
+
+function closeWindow(id) {
+    document.querySelector(id).remove();
+}
+function openWindow(id) {
     var tmpitem = `
-        <div class="window">
-            <div class="controlbar">
-                <div>O</div>
-                <div style="float: right; margin-top: -1.1em;">X</div>
-            </div>
-            <div id="windowcontent">
-            </div>
+    <div class="window" id="_${id}">
+        <div class="controlbar">
+        <button>O</button>
+        <button style="float: right;" onclick="closeWindow('#_${id}')">X</button >
+        </div>
+        <div id="windowcontent">
+        </div>
 
-        </div>`
+    </div>`
 
-    var div = document.getElementById("screen").insertAdjacentHTML("beforeend", tmpitem.trim());    
+    var div = document.getElementById("screen").insertAdjacentHTML("beforeend", tmpitem.trim());
     const _window = document.querySelector(".window");
     const controlbar = document.querySelector(".controlbar");
 
-    function onDrag({movementX,movementY}){
+    function onDrag({ movementX, movementY }) {
         let getStyle = window.getComputedStyle(_window);
         let left = parseInt(getStyle.left);
         let top = parseInt(getStyle.top);
@@ -41,18 +45,18 @@ function openWindow(){
         _window.style.top = `${top + movementY}px`
 
     }
-    document.addEventListener("mousedown", ()=> {
-        controlbar.addEventListener("mousemove",onDrag);
+    document.addEventListener("mousedown", () => {
+        controlbar.addEventListener("mousemove", onDrag);
     });
-    document.addEventListener("mouseup", ()=> {
-        controlbar.removeEventListener("mousemove",onDrag);
+    document.addEventListener("mouseup", () => {
+        controlbar.removeEventListener("mousemove", onDrag);
     });
 
 }
 
 window.onload = init;
-function init(){
-    createIdItem("desktop", "images/icons/folder.png", "all", "openWindow()");
+function init() {
+    createIdItem("desktop", "images/icons/folder.png", "all", "openWindow(1)");
     createIdItem("desktop", "images/icons/folder.png", "Lupus", "");
     createIdItem("desktop", "images/icons/sten.png", "Lupus", "");
 
